33579f196a5fdad832de04104073c7b6112838b7
Mathias Bredholt <mathiasbredholt@gmail.com>
2020-03-29 19:55:17 -0400

    new folder structure

--- components/libmapper/component.mk
+++ components/libmapper/component.mk
@@ -1,3 +1,3 @@
 COMPONENT_SRCDIRS := compat_idf/src liblo/src libmapper/src zlib
 COMPONENT_EXTRA_INCLUDES := $(COMPONENT_PATH)/compat_idf/include $(COMPONENT_PATH)/zlib $(COMPONENT_PATH)/liblo $(COMPONENT_PATH)/libmapper/include
-CPPFLAGS += -DHAVE_CONFIG_H -include compat_idf/include/compat.h
+CPPFLAGS += -DHAVE_CONFIG_H -include $(COMPONENT_PATH)/compat_idf/include/compat.h
