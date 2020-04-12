930f34c51a8eb43b122c6d855ec0c6a89be70484
mathiasbredholt <mathiasbredholt@gmail.com>
2020-04-12 17:54:02 -0400

    Added liblo config step to Makefile

--- components/libmapper/component.mk
+++ components/libmapper/component.mk
@@ -7,3 +7,10 @@ COMPONENT_OBJS := $(COMPAT_OBJS) $(LIBLO_OBJS) $(LIBMAPPER_OBJS) zlib/crc32.o
 COMPONENT_PRIV_INCLUDEDIRS := compat_idf/include zlib
 COMPONENT_ADD_INCLUDEDIRS := libmapper/include liblo
 CPPFLAGS += -DHAVE_CONFIG_H -include $(COMPONENT_PATH)/compat_idf/include/compat.h -Wno-char-subscripts -Wno-maybe-uninitialized
+
+COMPONENT_EXTRA_CLEAN := liblo/lo/lo.h
+
+liblo_config.o : lo.h
+
+lo.h: $(COMPONENT_PATH)/liblo/lo/lo.h
+    cd $(COMPONENT_PATH)/liblo && ./autogen.sh
